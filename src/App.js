6a19ec40c79a5306938fc987ab77391373da596c
Leandro Machado <leandrovictoram@gmail.com>
2023-05-17 14:11:09 -0300

    alteração 3

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
     {
       nome: 'Amor de Cristo refletido em relacionamentos amorosos',
       corPrimaria: '#3791a6',
-      corSecundaria: '#3791a6'
-    },
-    {
-      nome: 'Amor de Cristo para com pecadores',
-      corPrimaria: '#2b558c',
-      corSecundaria: '#2b558c'
-    }
+      corSecundaria: '#3791a6'}
+    // },
+    // {
+    //   nome: 'Amor de Cristo para com pecadores',
+    //   corPrimaria: '#2b558c',
+    //   corSecundaria: '#2b558c'
+    // }
   ]
 
   return (
